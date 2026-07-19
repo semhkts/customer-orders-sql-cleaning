9ee987660479d224025b981f21a05daa885ae25b
semhkts <38770811+semhkts@users.noreply.github.com>
2026-07-19 22:50:17 +1000

    Create 03_data_cleaning.sql

---
+++ sql/03_data_cleaning.sql
@@ -0,0 +1,14 @@
+/*
+Purpose:
+Clean and standardise the dataset.
+*/
+
+/*
+Tasks
+
+- Remove duplicates
+- Fix spelling mistakes
+- Standardise city names
+- Convert dates
+- Handle missing values
+*/
