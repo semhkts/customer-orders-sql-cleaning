55c8700bf029bbc805ad00b8951320d2102afe2c
semhkts <38770811+semhkts@users.noreply.github.com>
2026-07-19 22:46:46 +1000

    Update 01_create_database.sql

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,1 +1,22 @@
 -- Creates the database and raw customer orders table.
+/*
+Author: Sam
+Project: Customer Orders Data Cleaning
+Purpose: Create the database and raw table.
+*/
+
+CREATE DATABASE customer_orders_project;
+
+USE customer_orders_project;
+
+CREATE TABLE raw_customer_orders (
+    order_id INT,
+    customer_name VARCHAR(100),
+    email VARCHAR(150),
+    city VARCHAR(100),
+    order_date VARCHAR(50),
+    product VARCHAR(100),
+    quantity INT,
+    unit_price DECIMAL(10,2),
+    payment_status VARCHAR(50)
+);
